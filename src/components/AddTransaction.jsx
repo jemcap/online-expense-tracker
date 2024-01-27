ab3736e158978f5227ca7b85d25604d6b6f02079
JCapito <joshuaecapito22@gmail.com>
2024-01-27 16:18:09 +0000

    Implemented Logic to AddTransaction component to record the values inputted in each text box

--- src/components/AddTransaction.jsx
+++ src/components/AddTransaction.jsx
@@ -1,18 +1,23 @@
+import { useState } from "react";
 const AddTransaction = () => {
+  const [text, setText] = useState(0);
+  const [amount, setAmount] = useState(0);
+  
   return (
     <div>
       <h3>Add new transaction</h3>
       <form>
         <div className="form-control">
           <label>Text</label>
-          <input type="text" id="text" placeholder="Enter text..." />
+          <input type="text" value={text} onChange={(e) => setText(e.target.value)} id="text" placeholder="Enter text..." />
+          
         </div>
         <div className="form-control">
           <label>
             Amount <br />
             (negative - expense, positive - income)
           </label>
-          <input type="number" id="amount" placeholder="Enter amount..." />
+          <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} id="amount" placeholder="Enter amount..." />
         </div>
         <button className="btn">Add transaction</button>
       </form>
